282a2a1dd49d8c21402574e853ad231fe606dfbb
ubq323 <ubq323>
2021-05-24 00:26:43 +0000

    create posts and threads

--- apioforum/forum.py
+++ apioforum/forum.py
@@ -2,7 +2,8 @@
 # currently there is only ever one forum however
 
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request,
+    g, redirect, url_for
 )
 from .db import get_db
 
@@ -13,3 +14,33 @@ def view_forum():
     db = get_db()
     threads = db.execute("SELECT * FROM threads ORDER BY updated DESC LIMIT 10;").fetchall()
     return render_template("view_forum.html",threads=threads)
+
+@bp.route("/create_thread",methods=("GET","POST"))
+def create_thread():
+    db = get_db()
+    if request.method == "POST":
+        title = request.form['title']
+        content = request.form['content']
+        err = None
+        if g.user is None:
+            err = "you need to be logged in to create a thread"
+        elif len(title.strip()) == 0 or len(content.strip()) == 0:
+            err = "title and content can't be empty"
+
+        if err is None:
+            cur = db.cursor()
+            cur.execute(
+                "INSERT INTO threads (title,creator,created,updated) VALUES (?,?,current_timestamp,current_timestamp);",
+                (title,g.user)
+            )
+            thread_id = cur.lastrowid
+            cur.execute(
+                "INSERT INTO posts (thread,created,author,content) VALUES (?,current_timestamp,?,?);",
+                (thread_id,g.user,content)
+            )
+            db.commit()
+            return redirect(url_for('thread.view_thread',thread_id=thread_id))
+        
+        
+    return render_template("create_thread.html")
+
--- apioforum/thread.py
+++ apioforum/thread.py
@@ -1,8 +1,11 @@
 # view posts in thread
 
 from flask import (
-    Blueprint, render_template, abort
+    Blueprint, render_template, abort, request, g, redirect,
+    url_for, flash
 )
+from markdown import markdown
+from markupsafe import escape
 from .db import get_db
 
 bp = Blueprint("thread", __name__, url_prefix="/thread")
@@ -18,5 +21,31 @@ def view_thread(thread_id):
             "SELECT * FROM posts WHERE thread = ? ORDER BY created ASC;",
             (thread_id,)
         ).fetchall()
-        return render_template("view_thread.html",posts=posts,thread=thread)
+        rendered_posts = [markdown(escape(q['content'])) for q in posts]
+        return render_template("view_thread.html",posts=posts,thread=thread,thread_id=thread_id,rendered_posts=rendered_posts)
 
+@bp.route("/<int:thread_id>/create_post", methods=("POST",))
+def create_post(thread_id):
+    if g.user is None:
+        flash("you need to log in before you can post")
+        return redirect(url_for('thread.view_thread',thread_id=thread_id))
+    else:
+        db = get_db()
+        content = request.form['content']
+        thread = db.execute("SELECT * FROM threads WHERE id = ?;",(thread_id,)).fetchone()
+        if len(content.strip()) == 0:
+            flash("you cannot post an empty message")
+        elif not thread:
+            flash("that thread does not exist")
+        else:
+            db.execute(
+                "INSERT INTO posts (thread,author,content,created) VALUES (?,?,?,current_timestamp);",
+                (thread_id,g.user,content)
+            )
+            db.execute(
+                "UPDATE threads SET updated = current_timestamp WHERE id = ?;",
+                (thread_id,)
+            )
+            db.commit()
+            flash("post posted postfully")
+    return redirect(url_for('thread.view_thread',thread_id=thread_id))
