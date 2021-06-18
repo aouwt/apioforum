4f75e30d9584566dca8f3c67a9451c22839d9da1
raven <raven>
2021-06-18 21:38:54 +0000

    recent posts

--- apioforum/user.py
+++ apioforum/user.py
@@ -15,5 +15,11 @@ def view_user(username):
     user = db.execute("SELECT * FROM users WHERE username = ?;",(username,)).fetchone()
     if user is None:
         abort(404)
+    posts = db.execute(
+            "SELECT * FROM posts WHERE author = ? ORDER BY created DESC LIMIT 25;",(username,)).fetchall()
+    rendered_posts = [render(post['content']) for post in posts]
     return render_template("view_user.html", 
-            user=user, rendered_bio=render(user['bio'] or "hail GEORGE"))
+            user=user, 
+            rendered_bio=render(user['bio'] or "hail GEORGE"), 
+            posts=posts,
+            rendered_posts=rendered_posts)
